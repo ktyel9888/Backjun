59719c79047fb81361b61abb1e27e4bec2c9cf5e
22200255 <22200255@handong.ac.kr>
2025-06-30 23:15:10 +0900

    06/30 백준.

---
+++ Cpp/Q1008.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A,B;
+    cin >> A >>B;
+    printf("%.9f\n", double(A)/double(B)); // 꼭 소수점을 만들어야할때는 답 뿐만 아니라 매개변수들도 자료형을 변환할것!
+
+}
