a758d58558e1afd671e31c8c533892be80769b0c
22200255 <22200255@handong.ac.kr>
2025-08-31 23:52:17 +0900

    0831

--- Cpp/Q10699.cpp
+++ Cpp/Q10699.cpp
@@ -1,12 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <ctime> // 시간 출력을 위한 타임 헤더.
+#include <cstdio> // printf를 사용하기 위한 헤더.
+
 using namespace std;
 
 //서울의 오늘 날짜를 출력하는 프로그램을 작성하시오.
 
-int main(){
+int main() {
     time_t now = time(0); // 현재 시간 가져오기
-    cout << now;
-
+    struct tm* t = localtime(&now);
+    printf("%d-%02d-%02d\n", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday); 
+    return 0;
 }
+
 
