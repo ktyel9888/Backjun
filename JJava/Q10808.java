8b5f4a5b06521ea94c65f1b2a741891d51fef03a
ktyel9888 <ktyel9888@naver.com>
2025-09-01 23:01:46 +0900

    Update Q10808.java

--- JJava/Q10808.java
+++ JJava/Q10808.java
@@ -2,6 +2,11 @@ package JJava;
 
 import java.util.*;
 
+/*입력
+첫째 줄에 단어 S가 주어진다. 단어의 길이는 100을 넘지 않으며, 알파벳 소문자로만 이루어져 있다.
+
+출력
+단어에 포함되어 있는 a의 개수, b의 개수, …, z의 개수를 공백으로 구분해서 출력한다. */
 
 public class Q10808 {
     public static void main(String[] args) {
